a21c43cbbf514cd59e7064e243dde8df65f9d66e
Matthew Kandiah <mj.kandiah@googlemail.com>
2023-04-26 21:31:35 +0100

    well I've cocked that up

--- chapter2/2.1/insertion_sort.cpp
+++ chapter2/2.1/insertion_sort.cpp
@@ -3,16 +3,17 @@
 #include <cstdint>
 #include <iostream>
 
-void insertion_sort(std::vector<std::int32_t>& values) {
-    for (auto i=1; i < values.size(); i++) {
-        int32_t key {values[i]};
-        auto j = i - 1;
-        while (j > 0 && values[j] > key) {
-            values[j + 1] = values[j];
-            j--;
-        }
-        values[j + 1] = key;
+void insertion_sort(std::vector<std::int32_t>& values, bool decreasing) {
+  for (auto i=1; i < values.size(); i++) {
+    int32_t key {values[i]};
+    auto j = i - 1;
+    bool condition { (!decreasing && values[j] > key) || (decreasing && values[j] < key)};
+    while (j >= 0 && condition) {
+      values[j + 1] = values[j];
+      j--;
     }
+    values[j + 1] = key;
+  }
 }
 
 void print_vector(const std::vector<std::int32_t>& values) {
@@ -21,11 +22,19 @@ void print_vector(const std::vector<std::int32_t>& values) {
   }
 }
 
+void print_test_outputs(std::vector<int32_t>& values) {
+  std::cout << "Before sort\n";
+  print_vector(values);
+  insertion_sort(values, false);
+  std::cout << "After ascending sort\n";
+  print_vector(values);
+  insertion_sort(values, true);
+  std::cout << "After descending sort\n";
+  print_vector(values);
+}
+
 int main() {
   std::vector<std::int32_t> test1 {2, 10, 5, 7};
-  std::cout << "Before sort" << '\n';
-  print_vector(test1);
-  insertion_sort(test1);
-  std::cout << "After ascending sort " << '\n';
-  print_vector(test1);
+  print_test_outputs(test1);
 }
+
