a6dcd290f3467dc7f7978048a9feda731378e588
Matthew Kandiah <mj.kandiah@googlemail.com>
2023-06-03 12:44:54 +0100

    more setup

--- chapter7/quicksort.c
+++ chapter7/quicksort.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
 
+void printArray(int* array, int arrayLength) {
+  for (int i = 0; i < arrayLength; i++) {
+    printf("%d ", array[i]);
+  }
+  printf("%c", '\n');
+}
+
+void quickSort(int* array, int arrayLength) {}
+
 int main() {
-  printf("testing Makefile\n");
+  int numbers[] = {5,4,3,2,1};
+  int numbersLength = sizeof(numbers) / sizeof(numbers[0]);
+  printf("Unsorted numbers\n\t");
+  printArray(numbers, numbersLength);
+  
+  quickSort(numbers, numbersLength);
+
+  printf("Sorted numbers\n\t");
+  printArray(numbers, numbersLength);
 }
