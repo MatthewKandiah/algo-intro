1508fab2fc5866e3dcbca65c86fa1543abb0aae7
Matthew Kandiah <mj.kandiah@googlemail.com>
2023-06-04 14:17:06 +0100

    formatting

--- chapter8/countingSort.c
+++ chapter8/countingSort.c
@@ -28,9 +28,11 @@ void countingSort(int *array, int arrayLength, int limit) {
   // i in array
 
   for (int i = arrayLength - 1; i >= 0; i--) {
-    // for each element in array, check how many element less than or equal to it exist in the array
-    // then insert element into result at the last index that it could appear at consistent with the previous result
-    // note - we have to shift some of the indices by 1 compared to the book because they've indexed arrays from 1 
+    // for each element in array, check how many element less than or equal to
+    // it exist in the array then insert element into result at the last index
+    // that it could appear at consistent with the previous result note - we
+    // have to shift some of the indices by 1 compared to the book because
+    // they've indexed arrays from 1
     result[countingArray[array[i]] - 1] = array[i];
     // update our counting array so repeated values are handled sensibly
     countingArray[array[i]] -= 1;
@@ -51,3 +53,4 @@ int main() {
   printf("Sorted numbers:\n\t");
   printArray(numbers, numbersLength);
 }
+
