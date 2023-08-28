9f6d4709e30ded9b510742c9f902af360a308d75
Matthew Kandiah <mj.kandiah@googlemail.com>
2023-08-28 13:24:57 +0100

    working zig insertion sort

--- zig/src/insertionSort.zig
+++ zig/src/insertionSort.zig
@@ -1,20 +1,83 @@
 const std = @import("std");
 
-fn insertionSort(_: []i32) void {
-    std.debug.print("runInsertionSort\n", .{});
-}
-
-fn randomiseIntegers(numbers: []i32) void {
-    const rng = std.rand.DefaultPrng.init(std.time.microTimestamp());
-    for (numbers) |*numberPtr| {
-        numberPtr.* = rng.random();
+fn insertionSort(numbers: []i32) void {
+    var valIdx: usize = 1;
+    while (valIdx < numbers.len) : (valIdx += 1) {
+        const val: i32 = numbers[valIdx];
+        var compIdx: usize = valIdx;
+        while (compIdx > 0) : (compIdx -= 1) {
+            if (numbers[compIdx - 1] > val) {
+                numbers[compIdx] = numbers[compIdx - 1];
+                numbers[compIdx - 1] = val;
+            } else {
+                break;
+            }
+        }
     }
 }
 
+fn randomiseIntegers(numbers: []i32, maxNumber: i32) void {
+    var rng = std.rand.DefaultPrng.init(@bitCast(std.time.microTimestamp()));
+    for (numbers) |*numberPtr| {
+        numberPtr.* = @mod(rng.random().int(i32), maxNumber + 1);
+    }
+}
+
+fn printNumbers(numbers: []i32) void {
+    for (numbers) |number| {
+        std.debug.print("{d}, ", .{number});
+    }
+    std.debug.print("\n", .{});
+}
+
 test "insertion sort - trivial case" {
-    var numbers = [_]i32 {1, 2, 3, 4, 5, 6, 7, 8 ,9};
+    var numbers = [_]i32{ 1, 2, 3, 4 };
     insertionSort(&numbers);
-    var expectedNumbers = [_]i32 {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    const expectedNumbers = [_]i32{ 1, 2, 3, 4 };
     try std.testing.expect(std.mem.eql(i32, &numbers, &expectedNumbers));
 }
 
+test "insertion sort - reverse case" {
+    var numbers = [_]i32{ 4, 3, 2, 1 };
+    insertionSort(&numbers);
+    const expectedNumbers = [_]i32{ 1, 2, 3, 4 };
+    try std.testing.expect(std.mem.eql(i32, &numbers, &expectedNumbers));
+}
+
+test "insertion sort - empty case" {
+    var numbers = [_]i32{};
+    insertionSort(&numbers);
+    const expectedNumbers = [_]i32{};
+    try std.testing.expect(std.mem.eql(i32, &numbers, &expectedNumbers));
+}
+
+test "insertion sort - single entry case" {
+    var numbers = [_]i32{5};
+    insertionSort(&numbers);
+    const expectedNumbers = [_]i32{5};
+    try std.testing.expect(std.mem.eql(i32, &numbers, &expectedNumbers));
+}
+
+test "insertion sort - random numbers" {
+    const DEBUG = false;
+    const RUN_NUMBER = 100;
+
+    for (0..RUN_NUMBER) |_| {
+        var numbers: [100]i32 = undefined;
+        randomiseIntegers(&numbers, 100);
+        if (DEBUG) {
+            std.debug.print("Before sorting: ", .{});
+            printNumbers(&numbers);
+        }
+
+        insertionSort(&numbers);
+        if (DEBUG) {
+            std.debug.print("After sorting: ", .{});
+            printNumbers(&numbers);
+        }
+
+        for (1..numbers.len) |idx| {
+            try std.testing.expect(numbers[idx] >= numbers[idx - 1]);
+        }
+    }
+}
