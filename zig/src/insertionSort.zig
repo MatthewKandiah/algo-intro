e8f8ad8f2caf7f4fe779b6f463c5d0ff2ade3916
Matthew Kandiah <mj.kandiah@googlemail.com>
2023-08-28 13:36:26 +0100

    better insertion sort

--- zig/src/insertionSort.zig
+++ zig/src/insertionSort.zig
@@ -5,13 +5,11 @@ fn insertionSort(numbers: []i32) void {
     while (valIdx < numbers.len) : (valIdx += 1) {
         const val: i32 = numbers[valIdx];
         var compIdx: usize = valIdx;
-        while (compIdx > 0) : (compIdx -= 1) {
-            if (numbers[compIdx - 1] > val) {
+        while (compIdx > 0 and numbers[compIdx - 1] > val) : (compIdx -= 1) {
                 numbers[compIdx] = numbers[compIdx - 1];
-                numbers[compIdx - 1] = val; // slightly wasteful to do this copy here
-            } else {
-                break;
-            }
+        }
+        if (compIdx != valIdx) {
+            numbers[compIdx] = val;
         }
     }
 }
