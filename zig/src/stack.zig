16707416ace3409036ba97063f1def4e769cc3d1
Matthew Kandiah <mj.kandiah@googlemail.com>
2023-08-31 21:08:07 +0100

    wip - generic struct set up

--- zig/src/stack.zig
+++ zig/src/stack.zig
@@ -1,27 +1,33 @@
 const std = @import("std");
 
-fn stack(comptime T: type, comptime size: usize) type {
+fn stack(comptime T: type,  comptime sz: usize) type {
     return struct {
-        const Self = @This();
-        data: [size]T,
-        head: usize,
+        data: []T,
+        head: usize = 0,
 
-        fn init() Self {
+        const Self = @This();
+        fn init(allocator: std.mem.Allocator) !Self {
             return Self {
-                .data = undefined,
-                .head = 0,
+                .data = try allocator.alloc(T, sz),
             };
         }
+
+        fn deinit(self: Self, allocator: std.mem.Allocator) void {
+            allocator.free(self.data);
+        }
     };
-} 
+}
 
 test "should make an empty stack of any size and type" {
-    const types = [_]type{i32, u16, c_char, f64};
+    const types = [_]type{i32, u16, c_char, f64, [*] u8, *[] const u8};
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
     inline for (0..100) |i| {
         const t = types[i % types.len];
-        const x = stack(t, i).init();
+        const x = try stack(t, i).init(allocator);
         try std.testing.expect(x.data.len == i);
         try std.testing.expect(x.head == 0);
+        x.deinit(allocator);
     }
 }
 
