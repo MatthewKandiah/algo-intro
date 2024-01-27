538a06fd7de06959e80845725405ecec3645b276
Matthew Kandiah <mj.kandiah@googlemail.com>
2024-01-27 21:04:47 +0000

    insertion sort

--- src/main.zig
+++ src/main.zig
@@ -5,19 +5,15 @@ pub fn main() void {
 }
 
 fn bubble_sort(comptime size: usize, comptime T: type, list: *[size]T) void {
-    var i: usize = 0;
-    while (i < list.len) {
-        var j: usize = 0;
-        while (j < list.len - 1 - i) {
+    for (0..list.len) |i| {
+        for (0..list.len - 1 - i) |j| {
             const left = list[j];
             const right = list[j + 1];
             if (left > right) {
                 list[j] = right;
                 list[j + 1] = left;
             }
-            j += 1;
         }
-        i += 1;
     }
 }
 
@@ -31,4 +27,27 @@ test "bubble sort should work" {
     var expectedResult_u8 = [_]u8{ 1, 2, 3, 4, 5 };
     bubble_sort(list_u8.len, u8, &list_u8);
     try std.testing.expectEqualSlices(u8, &expectedResult_u8, &list_u8);
+}
+
+fn insertion_sort(comptime size: usize, comptime T: type, list: *[size]T) void {
+    for (1..list.len) |i| {
+        const value = list[i];
+        var j: usize = 0;
+        while (j < i) {
+            if (list[i-1-j] > value) {
+                list[i-j] = list[i-1-j];
+                j += 1;
+            } else {
+                break;
+            }
+        }
+        list[i-j] = value;
+    }
+}
+
+test "insertion_sort should work" {
+    var list_i32 = [_]i32{ 5, 3, 2, 4, 1 };
+    var expectedResult_i32 = [_]i32{ 1, 2, 3, 4, 5 };
+    insertion_sort(list_i32.len, i32, &list_i32);
+    try std.testing.expectEqualSlices(i32, &expectedResult_i32, &list_i32);
 }
---
+++ src/notes.md
@@ -0,0 +1,10 @@
+# Sorting
+## Bubble sort
+- Really simple
+- Quadratic time complexity
+- Compare element `i` with element `i+1`, and swap if it is greater. This pushes maximum element to the final index. Shrink working array to not include maximum element and repeat. After each iteration the next largest value is added to the sort sub-array at the end of the target array. 
+
+## Insertion sort
+- Also pretty simple
+- Quadratic time complexity
+- Maintain a sorted sub-array on the left of the target array. Iterate backwards through the sorted sub-array, and insert the element into the correct position. The sorted sub-array is now one element longer. Repeat until the whole target array is sorted.
