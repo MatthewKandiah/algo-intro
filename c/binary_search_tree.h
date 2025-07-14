f0c027895ac61054c2c4b86d9ab381a2a3939415
Matthew Kandiah <mj.kandiah@googlemail.com>
2025-07-14 19:51:37 +0100

    count nodes function to verify deleting is working

--- c/binary_search_tree.h
+++ c/binary_search_tree.h
@@ -15,6 +15,7 @@ typedef struct Node {
 
 bool is_binary_search_tree(Node *);
 void print_in_order(Node *);
+int count_nodes_in_tree(Node *);
 Node *rec_search(Node *, int64_t);
 Node *itr_search(Node *, int64_t);
 Node *minimum(Node *);
@@ -22,5 +23,7 @@ Node *maximum(Node *);
 Node *successor(Node *);   // get the next node visited in an ordered walk
 Node *predecessor(Node *); // get the previous node visited in an ordered walk
 void tree_insert(BinarySearchTree *, Node *);
-void tree_transplant(BinarySearchTree*, Node *old_node, Node *new_node); // replaces one subtree as the child of its parent with another subtree
+void tree_transplant(BinarySearchTree *, Node *old_node,
+                     Node *new_node); // replaces one subtree as the child of
+                                      // its parent with another subtree
 void tree_delete(BinarySearchTree *, Node *);
