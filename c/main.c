3261ff1e60fd63bcdfa3fe0d403ebd1fe15b5055
Matthew Kandiah <mj.kandiah@googlemail.com>
2025-07-27 16:03:05 +0100

    typo

--- c/main.c
+++ c/main.c
@@ -129,7 +129,7 @@ int main(void) {
     free(random_ints);
   }
 
-  printf("single shorted path tests\n");
+  printf("single shortest path tests\n");
   {
     double weights[] = {0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8};
     Graph g = graph_create(5, 8, "0-1 1-2 2-3 3-4 4-3 3-2 2-1 1-0", weights);
